bae2f3e4a9f56970f19268811039a3bb030ceed2
Tamás Demeter-Haludka <yorirou@gmail.com>
2018-03-23 15:09:46 +0100

    Small fixes, adds verbose mode.

--- testrunner.go
+++ testrunner.go
@@ -33,37 +33,46 @@ var (
 	pattern = flag.String("pattern", "Test.php$", "pattern to match test files")
 	threads = flag.Int("threads", runtime.NumCPU(), "number of threads")
 	timeout = flag.Int("timeout", 9, "after timeout minutes, a new line will be printed to stdout")
+	verbose = flag.Bool("verbose", false, "enables verbose mode")
 
-	split   = regexp.MustCompile(`\s+`)
-	parts   []string
-	exit    = 0
-	exitMtx sync.Mutex
+	split     = regexp.MustCompile(`\s+`)
+	parts     []string
+	success   = 0
+	fail      = 0
+	resultMtx sync.Mutex
 )
 
 // worker reads from the channel, runs the command and puts the output on the output channel.
-func worker(path, output chan string) {
+func worker(path, output chan string, wg *sync.WaitGroup) {
 	for filename := range path {
-		output <- run(filename)
+		maybeLog("Starting file " + filename)
+		output <- run(filename, wg)
 	}
 }
 
 // run runs the command with the file in path as an extra parameter.
-func run(path string) string {
+func run(path string, wg *sync.WaitGroup) string {
+	defer wg.Done()
+
 	start := time.Now()
-	cmdparts := append(parts, path)
+	cmdparts := append(parts[:], path)
 
 	cmd := exec.Cmd{
 		Path: parts[0],
 		Args: cmdparts,
 	}
 	out, err := cmd.CombinedOutput()
+	resultMtx.Lock()
 	if err != nil {
-		exitMtx.Lock()
-		exit = 1
-		exitMtx.Unlock()
+		fail += 1
+	} else {
+		success += 1
 	}
+	resultMtx.Unlock()
 
-	return fmt.Sprintf("Running %s\n\n%s\n\nElapsed:%s\n",
+	maybeLog("Finished file " + path)
+
+	return fmt.Sprintf("Running %s\n\n%s\n\nElapsed: %s\n",
 		strings.Join(cmdparts, " "),
 		string(out),
 		time.Since(start).String(),
@@ -71,15 +80,22 @@ func run(path string) string {
 }
 
 // printer prints out the messages coming the channel, and marks the job done in the wait group.
-func printer(str chan string, wg *sync.WaitGroup) {
+func printer(str chan string) {
 	for {
 		select {
 		case output := <-str:
-			fmt.Printf("\n%s\n", output)
-			wg.Done()
+			if output = strings.TrimSpace(output); output != "" {
+				fmt.Printf("\n%s\n\n", output)
+			}
 		case <-time.After(time.Duration(*timeout) * time.Minute):
 			fmt.Println("")
 		}
+	}
+}
+
+func maybeLog(msg string) {
+	if *verbose {
+		fmt.Fprintln(os.Stderr, msg)
 	}
 }
 
@@ -98,10 +114,10 @@ func main() {
 	output := make(chan string)
 
 	for i := 0; i < *threads; i++ {
-		go worker(input, output)
+		go worker(input, output, &wg)
 	}
 
-	go printer(output, &wg)
+	go printer(output)
 
 	matcher, err := regexp.Compile(*pattern)
 	if err != nil {
@@ -112,11 +128,16 @@ func main() {
 	fmt.Printf("Starting %d threads\n\n", *threads)
 
 	start := time.Now()
+	files := make(map[string]struct{})
 
 	filepath.Walk(*root, func(path string, info os.FileInfo, err error) error {
 		if matcher.MatchString(path) {
-			wg.Add(1)
-			input <- path
+			if _, found := files[path]; !found {
+				wg.Add(1)
+				maybeLog("Adding file " + path)
+				files[path] = struct{}{}
+				input <- path
+			}
 		}
 
 		return nil
@@ -126,7 +147,9 @@ func main() {
 	close(input)
 	close(output)
 
-	fmt.Printf("\n\nComplete runtime: %s\n\n", time.Since(start).String())
+	fmt.Printf("\n\nComplete runtime: %s | Success: %d Failure: %d\n\n", time.Since(start).String(), success, fail)
 
-	os.Exit(exit)
+	if fail > 0 {
+		os.Exit(1)
+	}
 }
